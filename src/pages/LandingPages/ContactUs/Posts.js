7db41418a19b0780cd61bcc7438edf59e8ca535e
Nathan Lo <nate628426@gmail.com>
2022-01-23 02:30:11 -0500

    west lafayette finished

--- src/pages/LandingPages/ContactUs/Posts.js
+++ src/pages/LandingPages/ContactUs/Posts.js
@@ -23,13 +23,11 @@ import MKTypography from "components/MKTypography";
 
 // Material Kit 2 React components
 import TransparentBlogCard from "examples/Cards/BlogCards/TransparentBlogCard";
-import BackgroundBlogCard from "examples/Cards/BlogCards/BackgroundBlogCard";
 
 // Images
-import post1 from "assets/images/examples/testimonial-6-2.jpg";
-import post2 from "assets/images/examples/testimonial-6-3.jpg";
-import post3 from "assets/images/examples/blog-9-4.jpg";
-import post4 from "assets/images/examples/blog2.jpg";
+import post1 from "assets/images/statue.jpg";
+import post2 from "assets/images/isspurdue.jpg";
+import post3 from "assets/images/engineeringfountain.jpg";
 
 function Places() {
   return (
@@ -44,51 +42,39 @@ function Places() {
           <Grid item xs={12} sm={6} lg={3}>
             <TransparentBlogCard
               image={post1}
-              title="Rover raised $65 million"
-              description="Finding temporary housing for your dog should be as easy as renting an Airbnb. That’s the idea behind Rover ..."
+              title="Timelapse by John Purdue"
+              description="Beautiful image of the sky at Memorial Mall, taken by Trevor Mahlmann"
               action={{
                 type: "internal",
-                route: "/pages/blogs/author",
+                route: "pages/LandingPages/ContactUs/Posts",
                 color: "info",
-                label: "read more",
+                label: "Submit your own image",
               }}
             />
           </Grid>
           <Grid item xs={12} sm={6} lg={3}>
             <TransparentBlogCard
               image={post2}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
+              title="The International Space Station Over Purdue"
+              description="taken by Dave Wegiel Photography"
               action={{
                 type: "internal",
-                route: "/pages/blogs/author",
+                route: "pages/LandingPages/ContactUs/Posts",
                 color: "info",
-                label: "read more",
+                label: "Submit your own image",
               }}
             />
           </Grid>
           <Grid item xs={12} sm={6} lg={3}>
             <TransparentBlogCard
               image={post3}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
+              title="Engineering Fountain Timelapse"
+              description="A beautiful timelapse of the sky by Purdue's famous engineering fountain, taken by Trevor Mahlmann."
               action={{
                 type: "internal",
-                route: "/pages/blogs/author",
+                route: "pages/LandingPages/ContactUs/Posts",
                 color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <BackgroundBlogCard
-              image={post4}
-              title="Flexible work hours"
-              description="Rather than worrying about switching offices every couple years, you stay in the same place."
-              action={{
-                type: "internal",
-                route: "/pages/blogs/author",
-                label: "read more",
+                label: "Submit your own image",
               }}
             />
           </Grid>
