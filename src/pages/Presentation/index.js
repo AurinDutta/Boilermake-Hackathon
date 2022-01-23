9ab3700d52ad4b701f68b41265d5f75b514628e2
Aurin Dutta <dutta36@purdue.edu>
2022-01-22 23:59:13 -0500

    Update index.js

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -14,36 +14,50 @@ Coded by www.creative-tim.com
 */
 
 // @mui material components
+/*
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
+*/
 
 // Material Kit 2 React components
+/*
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 import MKSocialButton from "components/MKSocialButton";
+*/
 
 // Material Kit 2 React examples
 import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+/*
 import DefaultFooter from "examples/Footers/DefaultFooter";
 import FilledInfoCard from "examples/Cards/InfoCards/FilledInfoCard";
+*/
 
+/*
 // Presentation page sections
 import Information from "pages/Presentation/sections/Information";
 import DesignBlocks from "pages/Presentation/sections/DesignBlocks";
 import Pages from "pages/Presentation/sections/Pages";
 import Testimonials from "pages/Presentation/sections/Testimonials";
 import Download from "pages/Presentation/sections/Download";
+*/
 
+/*
 // Presentation page components
 import BuiltByDevelopers from "pages/Presentation/components/BuiltByDevelopers";
+*/
 
 // Routes
 import routes from "routes";
+/*
 import footerRoutes from "footer.routes";
+*/
 
 // Images
+/*
 import bgImage from "assets/images/bg-presentation.jpg";
+*/
 
 function Presentation() {
   return (
@@ -58,6 +72,7 @@ function Presentation() {
         }}
         sticky
       />
+      {/*
       <MKBox
         minHeight="75vh"
         width="100%"
@@ -216,6 +231,7 @@ function Presentation() {
       <MKBox pt={6} px={1} mt={6}>
         <DefaultFooter content={footerRoutes} />
       </MKBox>
+              */}
     </>
   );
 }
