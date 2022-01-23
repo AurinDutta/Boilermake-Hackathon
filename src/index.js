7c6e802843b68a00dafc57aabb3cd02473ad41c3
Aurin Dutta <dutta36@purdue.edu>
2022-01-22 20:39:34 -0500

    Got the template set up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/**
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
 
 ReactDOM.render(
-  
+  <BrowserRouter>
     <App />
-  ,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-
-reportWebVitals();
